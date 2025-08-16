877640a5600c4d09c466993ef0e492e6f3d76d2b
azeyyy0729 <lucas.morel.play@gmail.com>
2025-08-16 18:15:39 +0200

    Update template.sql

--- template.sql
+++ template.sql
@@ -15,7 +15,7 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Listage de la structure de table esxlegacy_4c08fb. addon_account
+-- Listage de la structure de table esxlegacy_5828d8. addon_account
 CREATE TABLE IF NOT EXISTS `addon_account` (
   `name` varchar(60) NOT NULL,
   `label` varchar(100) NOT NULL,
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS `addon_account` (
   PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
--- Listage des données de la table esxlegacy_4c08fb.addon_account : ~8 rows (environ)
+-- Listage des données de la table esxlegacy_5828d8.addon_account : ~8 rows (environ)
 INSERT INTO `addon_account` (`name`, `label`, `shared`) VALUES
 	('bank_savings', 'Savings account', 0),
 	('caution', 'caution', 0),
@@ -34,7 +34,7 @@ INSERT INTO `addon_account` (`name`, `label`, `shared`) VALUES
 	('society_police', 'Police', 1),
 	('society_taxi', 'Taxi', 1);
 
--- Listage de la structure de table esxlegacy_4c08fb. addon_account_data
+-- Listage de la structure de table esxlegacy_5828d8. addon_account_data
 CREATE TABLE IF NOT EXISTS `addon_account_data` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `account_name` varchar(100) DEFAULT NULL,
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS `addon_account_data` (
   KEY `index_addon_account_data_account_name` (`account_name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
--- Listage des données de la table esxlegacy_4c08fb.addon_account_data : ~8 rows (environ)
+-- Listage des données de la table esxlegacy_5828d8.addon_account_data : ~8 rows (environ)
 INSERT INTO `addon_account_data` (`id`, `account_name`, `money`, `owner`) VALUES
 	(1, 'society_cardealer', 0, NULL),
 	(2, 'society_police', 0, NULL),
@@ -56,7 +56,7 @@ INSERT INTO `addon_account_data` (`id`, `account_name`, `money`, `owner`) VALUES
 	(10, 'bank_savings', 0, '1a4d1eac2a054a94f120e91b5f635340b8e3c169'),
 	(11, 'caution', 0, '1a4d1eac2a054a94f120e91b5f635340b8e3c169');
 
--- Listage de la structure de table esxlegacy_4c08fb. addon_inventory
+-- Listage de la structure de table esxlegacy_5828d8. addon_inventory
 CREATE TABLE IF NOT EXISTS `addon_inventory` (
   `name` varchar(60) NOT NULL,
   `label` varchar(100) NOT NULL,
@@ -64,7 +64,7 @@ CREATE TABLE IF NOT EXISTS `addon_inventory` (
   PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
--- Listage des données de la table esxlegacy_4c08fb.addon_inventory : ~5 rows (environ)
+-- Listage des données de la table esxlegacy_5828d8.addon_inventory : ~5 rows (environ)
 INSERT INTO `addon_inventory` (`name`, `label`, `shared`) VALUES
 	('society_ambulance', 'EMS', 1),
 	('society_cardealer', 'Cardealer', 1),
@@ -72,7 +72,7 @@ INSERT INTO `addon_inventory` (`name`, `label`, `shared`) VALUES
 	('society_police', 'Police', 1),
 	('society_taxi', 'Taxi', 1);
 
--- Listage de la structure de table esxlegacy_4c08fb. addon_inventory_items
+-- Listage de la structure de table esxlegacy_5828d8. addon_inventory_items
 CREATE TABLE IF NOT EXISTS `addon_inventory_items` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `inventory_name` varchar(100) NOT NULL,
@@ -85,9 +85,9 @@ CREATE TABLE IF NOT EXISTS `addon_inventory_items` (
   KEY `index_addon_inventory_inventory_name` (`inventory_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
--- Listage des données de la table esxlegacy_4c08fb.addon_inventory_items : ~0 rows (environ)
+-- Listage des données de la table esxlegacy_5828d8.addon_inventory_items : ~0 rows (environ)
 
--- Listage de la structure de table esxlegacy_4c08fb. banking
+-- Listage de la structure de table esxlegacy_5828d8. banking
 CREATE TABLE IF NOT EXISTS `banking` (
   `identifier` varchar(46) DEFAULT NULL,
   `type` varchar(50) DEFAULT NULL,
@@ -97,11 +97,11 @@ CREATE TABLE IF NOT EXISTS `banking` (
   `balance` int(11) DEFAULT 0,
   `label` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Listage des données de la table esxlegacy_4c08fb.banking : ~0 rows (environ)
+-- Listage des données de la table esxlegacy_5828d8.banking : ~0 rows (environ)
 
--- Listage de la structure de table esxlegacy_4c08fb. billing
+-- Listage de la structure de table esxlegacy_5828d8. billing
 CREATE TABLE IF NOT EXISTS `billing` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `identifier` varchar(60) NOT NULL,
@@ -113,9 +113,9 @@ CREATE TABLE IF NOT EXISTS `billing` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
--- Listage des données de la table esxlegacy_4c08fb.billing : ~0 rows (environ)
+-- Listage des données de la table esxlegacy_5828d8.billing : ~0 rows (environ)
 
--- Listage de la structure de table esxlegacy_4c08fb. datastore
+-- Listage de la structure de table esxlegacy_5828d8. datastore
 CREATE TABLE IF NOT EXISTS `datastore` (
   `name` varchar(60) NOT NULL,
   `label` varchar(100) NOT NULL,
@@ -123,7 +123,7 @@ CREATE TABLE IF NOT EXISTS `datastore` (
   PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
--- Listage des données de la table esxlegacy_4c08fb.datastore : ~9 rows (environ)
+-- Listage des données de la table esxlegacy_5828d8.datastore : ~9 rows (environ)
 INSERT INTO `datastore` (`name`, `label`, `shared`) VALUES
 	('property', 'Property', 0),
 	('society_ambulance', 'EMS', 1),
@@ -135,7 +135,7 @@ INSERT INTO `datastore` (`name`, `label`, `shared`) VALUES
 	('user_helmet', 'Helmet', 0),
 	('user_mask', 'Mask', 0);
 
--- Listage de la structure de table esxlegacy_4c08fb. datastore_data
+-- Listage de la structure de table esxlegacy_5828d8. datastore_data
 CREATE TABLE IF NOT EXISTS `datastore_data` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(60) NOT NULL,
@@ -146,7 +146,7 @@ CREATE TABLE IF NOT EXISTS `datastore_data` (
   KEY `index_datastore_data_name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
--- Listage des données de la table esxlegacy_4c08fb.datastore_data : ~11 rows (environ)
+-- Listage des données de la table esxlegacy_5828d8.datastore_data : ~11 rows (environ)
 INSERT INTO `datastore_data` (`id`, `name`, `owner`, `data`) VALUES
 	(1, 'society_police', NULL, '{}'),
 	(2, 'society_ambulance', NULL, '{}'),
@@ -160,7 +160,7 @@ INSERT INTO `datastore_data` (`id`, `name`, `owner`, `data`) VALUES
 	(10, 'user_helmet', '1a4d1eac2a054a94f120e91b5f635340b8e3c169', '{}'),
 	(11, 'user_mask', '1a4d1eac2a054a94f120e91b5f635340b8e3c169', '{}');
 
--- Listage de la structure de table esxlegacy_4c08fb. fine_types
+-- Listage de la structure de table esxlegacy_5828d8. fine_types
 CREATE TABLE IF NOT EXISTS `fine_types` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `label` varchar(255) DEFAULT NULL,
@@ -169,7 +169,7 @@ CREATE TABLE IF NOT EXISTS `fine_types` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
--- Listage des données de la table esxlegacy_4c08fb.fine_types : ~52 rows (environ)
+-- Listage des données de la table esxlegacy_5828d8.fine_types : ~52 rows (environ)
 INSERT INTO `fine_types` (`id`, `label`, `amount`, `category`) VALUES
 	(1, 'Misuse of a horn', 30, 0),
 	(2, 'Illegally Crossing a continuous Line', 40, 0),
@@ -224,7 +224,7 @@ INSERT INTO `fine_types` (`id`, `label`, `amount`, `category`) VALUES
 	(51, 'Involuntary manslaughter', 1800, 3),
 	(52, 'Fraud', 2000, 2);
 
--- Listage de la structure de table esxlegacy_4c08fb. items
+-- Listage de la structure de table esxlegacy_5828d8. items
 CREATE TABLE IF NOT EXISTS `items` (
   `name` varchar(50) NOT NULL,
   `label` varchar(50) NOT NULL,
@@ -234,7 +234,7 @@ CREATE TABLE IF NOT EXISTS `items` (
   PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
--- Listage des données de la table esxlegacy_4c08fb.items : ~33 rows (environ)
+-- Listage des données de la table esxlegacy_5828d8.items : ~33 rows (environ)
 INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
 	('alive_chicken', 'Living chicken', 1, 0, 1),
 	('bandage', 'Bandage', 2, 0, 1),
@@ -270,7 +270,7 @@ INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
 	('wood', 'Wood', 1, 0, 1),
 	('wool', 'Wool', 1, 0, 1);
 
--- Listage de la structure de table esxlegacy_4c08fb. jobs
+-- Listage de la structure de table esxlegacy_5828d8. jobs
 CREATE TABLE IF NOT EXISTS `jobs` (
   `name` varchar(50) NOT NULL,
   `label` varchar(50) DEFAULT NULL,
@@ -278,7 +278,7 @@ CREATE TABLE IF NOT EXISTS `jobs` (
   PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
--- Listage des données de la table esxlegacy_4c08fb.jobs : ~14 rows (environ)
+-- Listage des données de la table esxlegacy_5828d8.jobs : ~14 rows (environ)
 INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
 	('ambulance', 'EMS', 0),
 	('banker', 'Banker', 0),
@@ -295,7 +295,7 @@ INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
 	('taxi', 'Taxi', 0),
 	('unemployed', 'Unemployed', 0);
 
--- Listage de la structure de table esxlegacy_4c08fb. job_grades
+-- Listage de la structure de table esxlegacy_5828d8. job_grades
 CREATE TABLE IF NOT EXISTS `job_grades` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `job_name` varchar(50) DEFAULT NULL,
@@ -308,7 +308,7 @@ CREATE TABLE IF NOT EXISTS `job_grades` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
--- Listage des données de la table esxlegacy_4c08fb.job_grades : ~36 rows (environ)
+-- Listage des données de la table esxlegacy_5828d8.job_grades : ~36 rows (environ)
 INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
 	(1, 'unemployed', 0, 'unemployed', 'Unemployed', 200, '{}', '{}'),
 	(2, 'police', 0, 'recruit', 'Recruit', 20, '{}', '{}'),
@@ -347,14 +347,14 @@ INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, 
 	(39, 'banker', 3, 'trader', 'Broker', 40, '{}', '{}'),
 	(40, 'banker', 4, 'boss', 'Boss', 0, '{}', '{}');
 
--- Listage de la structure de table esxlegacy_4c08fb. licenses
+-- Listage de la structure de table esxlegacy_5828d8. licenses
 CREATE TABLE IF NOT EXISTS `licenses` (
   `type` varchar(60) NOT NULL,
   `label` varchar(60) NOT NULL,
   PRIMARY KEY (`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
--- Listage des données de la table esxlegacy_4c08fb.licenses : ~7 rows (environ)
+-- Listage des données de la table esxlegacy_5828d8.licenses : ~7 rows (environ)
 INSERT INTO `licenses` (`type`, `label`) VALUES
 	('boat', 'Boat License'),
 	('dmv', 'Driving Permit'),
@@ -364,7 +364,7 @@ INSERT INTO `licenses` (`type`, `label`) VALUES
 	('weapon', 'Weapon License'),
 	('weed_processing', 'Weed Processing License');
 
--- Listage de la structure de table esxlegacy_4c08fb. multicharacter_slots
+-- Listage de la structure de table esxlegacy_5828d8. multicharacter_slots
 CREATE TABLE IF NOT EXISTS `multicharacter_slots` (
   `identifier` varchar(60) NOT NULL,
   `slots` int(11) NOT NULL,
@@ -372,9 +372,9 @@ CREATE TABLE IF NOT EXISTS `multicharacter_slots` (
   KEY `slots` (`slots`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
--- Listage des données de la table esxlegacy_4c08fb.multicharacter_slots : ~0 rows (environ)
+-- Listage des données de la table esxlegacy_5828d8.multicharacter_slots : ~0 rows (environ)
 
--- Listage de la structure de table esxlegacy_4c08fb. owned_vehicles
+-- Listage de la structure de table esxlegacy_5828d8. owned_vehicles
 CREATE TABLE IF NOT EXISTS `owned_vehicles` (
   `owner` varchar(60) DEFAULT NULL,
   `plate` varchar(12) NOT NULL,
@@ -390,9 +390,9 @@ CREATE TABLE IF NOT EXISTS `owned_vehicles` (
   PRIMARY KEY (`plate`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
--- Listage des données de la table esxlegacy_4c08fb.owned_vehicles : ~0 rows (environ)
+-- Listage des données de la table esxlegacy_5828d8.owned_vehicles : ~0 rows (environ)
 
--- Listage de la structure de table esxlegacy_4c08fb. rented_vehicles
+-- Listage de la structure de table esxlegacy_5828d8. rented_vehicles
 CREATE TABLE IF NOT EXISTS `rented_vehicles` (
   `vehicle` varchar(60) NOT NULL,
   `plate` varchar(12) NOT NULL,
@@ -403,9 +403,9 @@ CREATE TABLE IF NOT EXISTS `rented_vehicles` (
   PRIMARY KEY (`plate`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
--- Listage des données de la table esxlegacy_4c08fb.rented_vehicles : ~0 rows (environ)
+-- Listage des données de la table esxlegacy_5828d8.rented_vehicles : ~0 rows (environ)
 
--- Listage de la structure de table esxlegacy_4c08fb. society_moneywash
+-- Listage de la structure de table esxlegacy_5828d8. society_moneywash
 CREATE TABLE IF NOT EXISTS `society_moneywash` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `identifier` varchar(60) NOT NULL,
@@ -414,9 +414,9 @@ CREATE TABLE IF NOT EXISTS `society_moneywash` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
--- Listage des données de la table esxlegacy_4c08fb.society_moneywash : ~0 rows (environ)
+-- Listage des données de la table esxlegacy_5828d8.society_moneywash : ~0 rows (environ)
 
--- Listage de la structure de table esxlegacy_4c08fb. users
+-- Listage de la structure de table esxlegacy_5828d8. users
 CREATE TABLE IF NOT EXISTS `users` (
   `identifier` varchar(60) NOT NULL,
   `accounts` longtext DEFAULT NULL,
@@ -448,11 +448,11 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
--- Listage des données de la table esxlegacy_4c08fb.users : ~1 rows (environ)
+-- Listage des données de la table esxlegacy_5828d8.users : ~1 rows (environ)
 INSERT INTO `users` (`identifier`, `accounts`, `group`, `inventory`, `job`, `job_grade`, `loadout`, `metadata`, `position`, `firstname`, `lastname`, `dateofbirth`, `sex`, `height`, `skin`, `status`, `is_dead`, `id`, `disabled`, `last_property`, `created_at`, `last_seen`, `phone_number`, `pincode`, `job2`, `job_grade2`) VALUES
 	('1a4d1eac2a054a94f120e91b5f635340b8e3c169', '{"bank":2500,"money":2500,"black_money":0}', 'admin', '[{"count":2500,"name":"money","slot":1}]', 'unemployed', 0, '[]', '{"lastPlaytime":351,"health":200,"jobDuty":false,"armor":0}', '{"y":-2757.454833984375,"z":21.3436279296875,"heading":331.6535339355469,"x":-1049.4066162109376}', 'Ancelotti', 'Carlos', '933976800', 'm', 180, '{"props":[{"drawable":-1,"texture":-1,"prop_id":0},{"drawable":-1,"texture":-1,"prop_id":1},{"drawable":-1,"texture":-1,"prop_id":2},{"drawable":-1,"texture":-1,"prop_id":6},{"drawable":-1,"texture":-1,"prop_id":7}],"model":"mp_m_freemode_01","tattoos":[],"hair":{"style":0,"color":0,"highlight":0,"texture":0},"headBlend":{"skinSecond":0,"skinFirst":0,"shapeFirst":0,"shapeSecond":0,"skinMix":0,"thirdMix":0,"shapeThird":0,"shapeMix":0,"skinThird":0},"faceFeatures":{"eyesOpening":0,"cheeksWidth":0,"noseWidth":0,"eyeBrownHigh":0,"neckThickness":0,"chinBoneLenght":0,"cheeksBoneWidth":0,"jawBoneWidth":0,"eyeBrownForward":0,"noseBoneHigh":0,"nosePeakHigh":0,"nosePeakSize":0,"jawBoneBackSize":0,"lipsThickness":0,"chinBoneLowering":0,"nosePeakLowering":0,"noseBoneTwist":0,"chinBoneSize":0,"chinHole":0,"cheeksBoneHigh":0},"headOverlays":{"beard":{"opacity":0,"color":0,"style":0,"secondColor":0},"chestHair":{"opacity":0,"color":0,"style":0,"secondColor":0},"bodyBlemishes":{"opacity":0,"color":0,"style":0,"secondColor":0},"complexion":{"opacity":0,"color":0,"style":0,"secondColor":0},"ageing":{"opacity":0,"color":0,"style":0,"secondColor":0},"moleAndFreckles":{"opacity":0,"color":0,"style":0,"secondColor":0},"sunDamage":{"opacity":0,"color":0,"style":0,"secondColor":0},"eyebrows":{"opacity":0,"color":0,"style":0,"secondColor":0},"lipstick":{"opacity":0,"color":0,"style":0,"secondColor":0},"blemishes":{"opacity":0,"color":0,"style":0,"secondColor":0},"blush":{"opacity":0,"color":0,"style":0,"secondColor":0},"makeUp":{"opacity":0,"color":0,"style":0,"secondColor":0}},"eyeColor":0,"components":[{"drawable":0,"texture":0,"component_id":0},{"drawable":0,"texture":0,"component_id":1},{"drawable":0,"texture":0,"component_id":2},{"drawable":0,"texture":0,"component_id":3},{"drawable":0,"texture":0,"component_id":4},{"drawable":0,"texture":0,"component_id":5},{"drawable":0,"texture":0,"component_id":6},{"drawable":0,"texture":0,"component_id":7},{"drawable":0,"texture":0,"component_id":8},{"drawable":0,"texture":0,"component_id":9},{"drawable":0,"texture":0,"component_id":10},{"drawable":0,"texture":0,"component_id":11}]}', '[{"name":"hunger","percent":97.02,"val":970200},{"name":"thirst","percent":97.765,"val":977650}]', 0, 3, 0, NULL, '2025-06-22 16:16:14', '2025-06-22 16:21:40', NULL, NULL, 'unemployed', 0);
 
--- Listage de la structure de table esxlegacy_4c08fb. user_licenses
+-- Listage de la structure de table esxlegacy_5828d8. user_licenses
 CREATE TABLE IF NOT EXISTS `user_licenses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(60) NOT NULL,
@@ -460,15 +460,15 @@ CREATE TABLE IF NOT EXISTS `user_licenses` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
--- Listage des données de la table esxlegacy_4c08fb.user_licenses : ~0 rows (environ)
+-- Listage des données de la table esxlegacy_5828d8.user_licenses : ~0 rows (environ)
 
--- Listage de la structure de table esxlegacy_4c08fb. whitelist
+-- Listage de la structure de table esxlegacy_5828d8. whitelist
 CREATE TABLE IF NOT EXISTS `whitelist` (
   `identifier` varchar(60) NOT NULL,
   PRIMARY KEY (`identifier`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_uca1400_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- Listage des données de la table esxlegacy_4c08fb.whitelist : ~0 rows (environ)
+-- Listage des données de la table esxlegacy_5828d8.whitelist : ~0 rows (environ)
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
