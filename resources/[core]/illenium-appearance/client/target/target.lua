5196d920a81534a8a3e28398d53c44552fafbbe2
azeyyy0729 <lucas.morel.play@gmail.com>
2025-08-16 18:05:52 +0200

    Delete resources/[core]/illenium-appearance directory

--- resources/[core]/illenium-appearance/client/target/target.lua
+++
@@ -1,9 +0,0 @@
-Target = {}
-
-function Target.IsOX()
-    return GetResourceState("ox_target") ~= "missing"
-end
-
-function Target.IsQB()
-    return GetResourceState("qb-target") ~= "missing"
-end
