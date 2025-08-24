b1da23e1682f1a6c9b639250834865327900f784
azeyyy0729 <lucas.morel.play@gmail.com>
2025-08-25 01:14:09 +0200

    Add files via upload

---
+++ resources/[core]/randol_newspaper/shared.lua
@@ -0,0 +1,8 @@
+return {
+    EnableBlip = true, -- Ped blip
+    Ped = 'a_m_m_paparazzi_01',
+    PedCoords = vec4(-604.82, -926.45, 22.86, 181.1),
+    BikeModel = 'scorcher', -- Bicycle might be too slow, maybe a faggio? (scooter)
+    BikeSpawn = vec4(-602.74, -927.65, 23.86, 179.5),
+    Coords = vec3(-604.7268, -927.4593, 23.8637)
+}
