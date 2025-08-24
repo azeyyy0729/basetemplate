32734c437e0c6308dfe0b91a600f6929885d69b4
azeyyy0729 <lucas.morel.play@gmail.com>
2025-08-25 01:21:47 +0200

    Update shared.lua

--- resources/[core]/location/shared/shared.lua
+++ resources/[core]/location/shared/shared.lua
@@ -7,7 +7,7 @@ Config.Blips = {
 Config.Location = {
     ["Location 1"] = { -- Airport
         shop = vector3(-1038.2040, -2731.4744, 20.1693),
-        ped = vector4(-1038.4315, -2730.7380, 19.1693, 183.1109),
+        ped = vector4(-1038.4315, -2730.7380, 20.1693, 183.1109),
         spawn = vector4(-1030.7371, -2731.1560, 20.1065, 236.5129),
         vehicles = {
             {label = "BMX", name = "bmx", price = 4,  image = "https://i.ibb.co/cwmcTyF/BMX.webp"},
@@ -15,6 +15,7 @@ Config.Location = {
         },
     },
 }
+
 
 
 
